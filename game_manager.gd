f6752a25240ecc20b064108a6e85530a6b67bb1e
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2024-03-02 13:48:17 -0300

    End Trophy & Points Counter

--- game_manager.gd
+++ game_manager.gd
@@ -1,8 +1,15 @@
 extends Node
 
+@onready var label = %PointsLabel
+
 
 var points = 0
 
+
 func add_point():
 	points += 1
-	print(points)
+	label.text = "Points: " + str(points)
+
+
+func win():
+	print("Game Won!")
